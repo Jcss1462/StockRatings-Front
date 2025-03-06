ba7eb30d3d5db3894aa2d8ad87764f59290d4551
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-03-06 11:29:33 -0500

    Se hacen ajustes esteicos en el StockVIew.vue

--- src/domains/stocks/views/StockView.vue
+++ src/domains/stocks/views/StockView.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container mx-auto p-4">
       <!-- Row 1 -->
-      <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mb-4 pt-5 pb-10">
+      <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mb-5 mt-5 pt-5 pb-5">
         <!-- Columna Izquierda (Título) -->
         <div class="md:col-span-6 flex items-center">
-          <h1 class="text-2xl font-bold">Tendencias del Mercado y Mejor Inversión 📊</h1>
+          <h1 class="text-2xl font-bold lg:text-left sm:text-center sm:mb-7 lg:mb-0 w-full" >Tendencias del Mercado y Mejor Inversión 📊</h1>
         </div>
         
         <!-- Columna Derecha (Componente Extra) -->
